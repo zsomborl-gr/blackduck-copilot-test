c70a928cf30f4cd83cef42094fe1dbb10fc0e8ca
zsomborl-gr <151627093+zsomborl-gr@users.noreply.github.com>
2024-10-25 17:18:29 +0100

    Create FileSrcPairLoader.cs

---
+++ FileSrcPairLoader.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CodeSecurityGuardrails.FindingMatching;
+
+/// <summary>
+///     Loads file source code from disk
+/// </summary>
+public class FileSrcPairLoader : ISrcPairLoader
+{
+    private readonly string _baseRepoDir;
+    private readonly string _headRepoDir;
+
+    public FileSrcPairLoader(string baseRepoDir, string headRepoDir)
+    {
+        _baseRepoDir = baseRepoDir;
+        _headRepoDir = headRepoDir;
+    }
+
+    public async Task<(string BaseSrc, string HeadSrc)> Load(string baseFile, string headFile)
+    {
+        var baseFileSrc = string.Empty;
+        var baseFilePath = Path.Combine(_baseRepoDir, baseFile);
+        if (Path.Exists(baseFilePath))
+        {
+            baseFileSrc = await File.ReadAllTextAsync(baseFilePath);
+        }
+        var headFileSrc = string.Empty;
+        var headFilePath = Path.Combine(_headRepoDir, headFile);
+        if (Path.Exists(headFilePath))
+        {
+            headFileSrc = await File.ReadAllTextAsync(headFilePath);
+        }
+
+        return (baseFileSrc, headFileSrc);
+    }
+}
